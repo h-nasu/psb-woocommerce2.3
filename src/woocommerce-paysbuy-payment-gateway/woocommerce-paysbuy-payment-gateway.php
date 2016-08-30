89119e3ddde64f0f47506bd4a45a2a537b2092dc
Jon Randy <jonathan@paysbuy.com>
2016-08-30 12:16:25 +0700

    Small change to payment url generation

--- src/woocommerce-paysbuy-payment-gateway/woocommerce-paysbuy-payment-gateway.php
+++ src/woocommerce-paysbuy-payment-gateway/woocommerce-paysbuy-payment-gateway.php
@@ -196,9 +196,10 @@ function woocommerce_paysbuy_init() {
 		function process_payment($order_id) {
 		
 			$order = new WC_Order($order_id);
+			$url = $order->get_checkout_payment_url(true);
 			return array(
 				'result'		=> 'success',
-				'redirect'	=> add_query_arg('order', $order->id, add_query_arg('key', $order->order_key, get_permalink(woocommerce_get_page_id('pay'))))
+				'redirect'	=> $url
 			);
 
 		}
